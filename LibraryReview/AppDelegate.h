6dc86e9c3c8363ae7f50b7b4397c8b072f53264f
Taylor Mott <trm36@humboldt.edu>
2015-08-24 11:06:43 -0600

    Initial Commit

---
+++ LibraryReview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LibraryReview
+//
+//  Created by DevMountain on 8/24/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
